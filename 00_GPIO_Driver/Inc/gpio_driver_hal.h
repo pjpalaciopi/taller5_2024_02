e08b0e5c0923ce5c80440c15a5624a64b08a95ef
Pedro J Palacio P <pjpalaciopi@unal.edu.co>
2024-12-01 15:16:42 -0500

    stm32f4xx_hal.h
    primer parte del driver de los GPIO

---
+++ 00_GPIO_Driver/Inc/gpio_driver_hal.h
@@ -0,0 +1,15 @@
+/*
+ * gpio_driver_hal.h
+ *
+ *  Created on: Dec 1, 2024
+ *      Author: pjpalaciopi@unal.edu.co
+ */
+
+#ifndef GPIO_DRIVER_HAL_H_
+#define GPIO_DRIVER_HAL_H_
+
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+
+#endif /* GPIO_DRIVER_HAL_H_ */
