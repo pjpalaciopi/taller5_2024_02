6ca89c0faf8cbfb74925fcd769fc83e15e00cfa9
Pedro J Palacio P <pjpalaciopi@unal.edu.co>
2025-01-30 09:06:46 -0500

    update timer
    update timer

---
+++ 04_UpdateTimer/Src/main.c
@@ -0,0 +1,22 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : pjpalaciopi@unal.edu.co
+ * @brief          : Main program body
+ ******************************************************************************
+*/
+#include <stm32f4xx.h>
+#include <stdint.h>
+
+
+
+
+// Start main() function
+int main(void)
+{
+    /* Loop forever */
+	while(1){
+
+	}
+	return 0;
+}
